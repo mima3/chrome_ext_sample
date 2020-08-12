2365d6b079ed95e84b2d91d74c6f16971c18a267
mima3 <7877000+mima3@users.noreply.github.com>
2020-08-12 20:20:36 +0900

    プログラムでJavaScriptを実行する例を追加

--- 003_contents_script/contentScript.js
+++ 003_contents_script/contentScript.js
@@ -3,6 +3,7 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   console.log(message);
   console.log(sender);
   console.log(sendResponse);
+  document.body.style.backgroundColor = message.color;
   console.log(window.document.body.innerHTML);
   sendResponse(window.document.body.innerHTML);
 });
--- 003_contents_script/popup.js
+++ 003_contents_script/popup.js
@@ -1,10 +1,24 @@
 const btn = document.getElementById('btnTest');
-
+const btn2 = document.getElementById('btnTest2');
+/**
+ * 宣言されているコンテンツスクリプトを実行する
+ */
 btn.onclick = function(element) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-   chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+   chrome.tabs.sendMessage(tabs[0].id, {color: "red"}, function(response) {
+     // ポップアップ上で右クリックすることで開発者ツールが表示され、
+     // そこでログが確認可能です.
      console.log('Responce.....');
      console.log(response);
    });
  });
+};
+
+/**
+ * ウェブページ上でスクリプトを実行する.
+ */
+btn2.onclick = function(element) {
+  chrome.tabs.executeScript({
+    code: 'document.body.style.backgroundColor="orange"'
+  });
 };
