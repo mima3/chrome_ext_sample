d13ad959b940ecb06c1cec648a46c7955f7c2fa0
mima3 <7877000+mima3@users.noreply.github.com>
2020-08-16 12:58:58 +0900

    リファクタリング.

--- 008_nativemessage/ext/background.js
+++ 008_nativemessage/ext/background.js
@@ -1,20 +1,44 @@
 
 var port = null;
+connect();
+
+/**
+ * 接続処理
+ */
+function connect() {
+  if (port) {
+    return;
+  }
+  var hostName = "my.extension.test";
+  port = chrome.runtime.connectNative(hostName);
+
+  /**
+   * アプリケーションからのメッセージ処理
+   */
+  port.onMessage.addListener( function (rcv) {
+    console.log('onNativeMessage');
+    console.log(rcv);
+    chrome.runtime.sendMessage(rcv);
+  });
+
+  /**
+   * アプリケーションからの切断要求
+   */
+  port.onDisconnect.addListener( function () {
+    console.log('onDisconnect');
+    port = null;
+  });
+}
+
+/**
+ * popup.jsからのメッセージ処理
+ */
 chrome.runtime.onMessage.addListener(function(message, sender) {
   console.log(message);
   if (!port) {
-    var hostName = "my.extension.test";
-    port = chrome.runtime.connectNative(hostName);
-    port.onMessage.addListener( function (rcv) {
-      console.log('onNativeMessage');
-      console.log(rcv);
-      chrome.runtime.sendMessage(rcv);
-    });
-    port.onDisconnect.addListener( function () {
-      console.log('onDisconnect');
-      port = null;
-    });
+    connect();
   }
+  // NetiveMessageの送信
   port.postMessage({"text": message});
 });
 
